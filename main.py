f71098fb0f66337fd9af0c736e6df7841311ed16
Javier <jquappev@gmail.com>
2022-06-09 17:07:52 -0400

    Add support for full chapters and verse ranges

--- main.py
+++ main.py
@@ -14,14 +14,16 @@ from books import BIBLE_BOOKS
 BOT_TOKEN = "secret_token"
 TRANSLATION = "kjv"
 API_URL = "https://bible-api.com/"
+MAX_MESSAGE_LENGTH = 4096
 
 
 def start(update: Update, context: CallbackContext):
     context.bot.send_message(
         chat_id=update.effective_chat.id,
         text="I'm a bot in progress, please be patient.\n"
-             "I will try to reply with a bible verse whenever one is mentioned like 'Ephesians 6:11'.\n"
-             "For books with numbers like '2 Kings' type '2Kings'."
+             "I will try to reply with a bible verse whenever one is mentioned like *'John 3:16'*.\n"
+             "Get a range of verses with *'John 1:1-5'*.\n"
+             "Get a full chapter with *'John 1'*.\n"
              "For a list of books type */books*",
         parse_mode="markdown"
     )
@@ -33,15 +35,16 @@ def send_verse(update: Update, context: CallbackContext):
         if book.lower() in text:
             quoted_book = book
             list_text = text.split()
-            quoted_chapter = list_text[list_text.index(quoted_book.lower()) + 1]
-            if re.match("\d{1,2}[\-\:][\d-]{1,2}", quoted_chapter):
-                quoted_chapter = quoted_chapter.replace('-', ':')
-                result_verse = get_verse(quoted_book, quoted_chapter)
-                context.bot.send_message(
-                    chat_id=update.effective_chat.id,
-                    text=result_verse,
-                    parse_mode='markdown'
-                )
+            quoted_verse = list_text[list_text.index(quoted_book.lower()) + 1]
+            if re.match("\d{1,2}\:?\d{0,2}\-?\d{0,2}", quoted_verse):
+                result = get_verse(quoted_book, quoted_verse)
+                result_verse = [result[i:i+MAX_MESSAGE_LENGTH] for i in range(0, len(result), MAX_MESSAGE_LENGTH)]
+                for r in result_verse:
+                    context.bot.send_message(
+                        chat_id=update.effective_chat.id,
+                        text=r,
+                        parse_mode='markdown'
+                    )
 
 
 def send_books(update: Update, context: CallbackContext):
@@ -54,11 +57,11 @@ def send_books(update: Update, context: CallbackContext):
     )
 
 
-def get_verse(book, chapter):
-    response = requests.get(API_URL + book + chapter + "?translation=" + TRANSLATION)
+def get_verse(book, verse):
+    response = requests.get(API_URL + book + verse + "?translation=" + TRANSLATION)
     response_data = response.json()
-    verse_text = response_data['verses'][0]['text']
-    result = "*" + book + " " + chapter + ", " + TRANSLATION.upper() + "*\n" + verse_text
+    verse_text = response_data['text']
+    result = "*" + book + " " + verse + ", " + TRANSLATION.upper() + "*\n" + verse_text
     return result
 
 
